40a917bceae4939e2dee0e559a455b558697ada6
Nate Schroeder <n8dogg59@hotmail.com>
2021-05-23 10:25:00 -0500

    updated about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,7 +20,7 @@ function About() {
             </div>
             <div className="description">
                 <p>
-                I am an extremely self driven full-stack web developer that has worked remotely the last ten years and will thrive in the competitive and sometimes stressful environment web development. Experienced in working with the MERN stack, and as a result am proficient in JavaScript, React, Node, and Express. Excellent communicator that is passionate about coding and comfortable working remotely as part of a development team while bringing strong skills in problem solving that will help increase efficiency and productivity.
+                I am an extremely self-driven Full-Stack Web Developer who thrives in a competitive and challenging web development environment. Experienced working with MERN stack, resulting in proficiency with JavaScript, React, Node, and Express. Excellent communicator passionate about coding and comfortable working remotely as part of a development team while bringing strong skills in problem-solving that will help increase efficiency and productivity.
                 </p>
             </div>
         </div>
